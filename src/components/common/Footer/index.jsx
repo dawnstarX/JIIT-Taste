6f8b3f6658c3c2ff25ff70eaab41c3642d8bdd6f
Abhinav <dawnstar15072000@gmail.com>
2022-08-07 01:56:15 +0530

    final commit

---
+++ src/components/common/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../Logo';
+import './styles.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        We here at JIIT taste  , strive to serve you the best meal course you will ever want your toung to taste
+
+      </p>
+      <Logo />
+    </footer>
+  );
+};
+
+export default Footer;
